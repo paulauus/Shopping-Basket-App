29116ec8f0620a01cce58f75be013f7c94dead41
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-07-12 16:54:47 +0100

    First half of function

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -1,5 +1,19 @@
 def generate_invoice(receipt_string: str) -> str:
+    # Split the lines
+    lines = receipt_string.strip().split("\n")
+    total_price = 0
+    new_lines = []
 
+    for line in lines:
+        if line.startswith("Total:"):
+            total_price = float(line.strip().split('£')[1])
+        else:
+            line_parts = line.split("£")
+            if len(line_parts) == 2:
+                item_name = line_parts[0]
+                item_price = float(line_parts[1])
+                item_VAT_price = item_price / 1.2
+                new_lines.append(f"{item_name} - £{item_VAT_price:.2f}")
     return  # return the invoice string
 
 
